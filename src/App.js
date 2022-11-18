10b557a25f27412df13fc5f2b64e40dddbad9eaa
AltarKaratas <altar.karatas@gmail.com>
2022-11-18 23:58:30 +0300

    Team, Contacts, Invoices, Create user form done
    Invoices, Contacts and Manage Team pages are done using MUI Datagrid. Form created using Formik and yup

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ import TopBar from "./scenes/global/Topbar";
 import Dashboard from "./scenes/dashboard";
 
 import Sidebar from "./scenes/global/Sidebar";
-// import Team from './scenes/team';
-// import Invoices from './scenes/Invoices';
-// import Contacts from './scenes/contacts';
+import Contacts from "./scenes/contacts";
+import Team from "./scenes/team/index";
+import Invoices from "./scenes/invoice";
+import Form from './scenes/form';
 // import Bar from './scenes/bar';
-// import Form from './scenes/form';
 // import Line from './scenes/line';
 // import Pie from './scenes/pie';
 // import FAQ from './scenes/faq';
@@ -22,30 +22,30 @@ function App() {
   const [theme, colorMode] = useMode();
 
   return (
-      <ColorModeContext.Provider value={colorMode}>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <div className="app">
-            <Sidebar />
-            <main className="content">
-              <TopBar />
-              <Routes>
-                <Route path="/" element={<Dashboard />} />
-                {/* <Route path="/team" element={<Team />} />
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <Sidebar />
+          <main className="content">
+            <TopBar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/team" element={<Team />} /> */
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/invoices" element={<Invoices />} />
               <Route path="/form" element={<Form />} />
-              <Route path="/bar" element={<Bar />} />
+              {/*<Route path="/bar" element={<Bar />} />
               <Route path="/pie" element={<Pie />} />
               <Route path="/line" element={<Line />} />
               <Route path="/faq" element={<FAQ />} />
               <Route path="/geography" element={<Geography />} />
               <Route path="/calendar" element={<Calendar />} /> */}
-              </Routes>
-            </main>
-          </div>
-        </ThemeProvider>
-      </ColorModeContext.Provider>
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
